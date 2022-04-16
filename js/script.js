fadbb8c7fa9501ec43f47d2556768d8b88888930
Mateusz Kozlowsk <climatik@world.pl>
2022-04-16 22:57:49 +0200

    JS added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navigation = document.querySelector('.nav');
-const allNavigationItem = document.querySelectorAll(`.nav__item`)
+const allNavigationItem = document.querySelectorAll(`.nav__item`);
+const navBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const showNav = () => {
     navigation.classList.toggle('nav--active');
@@ -12,4 +15,24 @@ const showNav = () => {
     })
 }
 
+const burgerColor = () => {
+    let scrollSection = window.scrollY;
+
+    allSections.forEach(section => {
+        if (section.classList.contains('white') && section.offsetTop <= scrollSection + 60) {
+            navBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white') && section.offsetTop <= scrollSection + 60) {
+            navBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
+const showYear = () => {
+    let currentYear = (new Date).getFullYear();
+    footerYear.innerText = `${currentYear} `
+}
+
+showYear();
 burgerBtn.addEventListener('click', showNav);
+window.addEventListener('scroll', burgerColor)
+
